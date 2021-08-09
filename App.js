2c97a7bdc92c13522f12af975ff40743ab10b7de
Adharsh Sasidaran <78460735+ADHARSH-S@users.noreply.github.com>
2021-08-09 10:00:49 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,36 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, View,} from 'react-native';
+import AppButton from './app/component/AppButton';
+
+
+import ViewImagescreen from './screens/ViewImagescreen';
+import WelcomeScreen from './screens/WelcomeScreen';
+import Card from './app/component/card';
+import Cardlist from './screens/cardlist';
+import Carddetails from './screens/carddetails';
+import Profilelister from './app/component/profilelister';
+
+import MyAccountScreen from './screens/MyAccount';
+import Email from './app/component/Email';
+import MainScreen from './screens/MainScreen';
+import ProfileMessage from './app/component/ProfileMessage';
+
+
+
+export default function App() {
+  return (
+
+<ProfileMessage></ProfileMessage>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+   
+    
+    alignItems: 'center',
+    justifyContent: 'center',
+    alignContent:'center'
+  }
+});
